461dbd001ae7f3460bc2542f913384580dbea674
AhmedCode1996 <ahmedharb960@gmail.com>
2023-08-02 08:16:01 +0300

    implementing course card

--- src/pages/Course.jsx
+++ src/pages/Course.jsx
@@ -2,6 +2,11 @@
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
+import clock from "./../assets/clock.png";
+import user from "./../assets/user.png";
+import document from "./../assets/document.png";
+import star from "./../assets/star.png";
+
 import playButton from "./../assets/play.svg";
 import pauseButton from "./../assets/pause.svg";
 import fullScreenButton from "./../assets/fullScreen.svg";
@@ -12,7 +17,7 @@ import Video from "./../components/Video/Video";
 
 import { getAllCourses } from "../features/courseSlice";
 import { styled } from "styled-components";
-import { COLORS, FONT_FAMILY } from "../constants";
+import { COLORS, FONT_FAMILY, TYPOGRAPHY } from "../constants";
 import { useEffect, useRef, useState } from "react";
 
 const Course = () => {
@@ -24,7 +29,6 @@ const Course = () => {
   const { courseid } = useParams();
   const { courses } = useSelector(getAllCourses);
   const targetCourse = courses.find((course) => course.id === Number(courseid));
-
   const handlePlayPauseClick = () => {
     if (videoRef.current.paused) {
       videoRef.current.play();
@@ -60,37 +64,75 @@ const Course = () => {
   }, []);
 
   return (
-    <div>
-      <VideoWrapper>
-        <ControlBar>
-          <Play
+    <FullCourseWrapper>
+      <CourseWrapper>
+        <VideoWrapper>
+          <ControlBar>
+            <Play
+              onClick={handlePlayPauseClick}
+              src={!isPlaying ? playButton : pauseButton}
+            />
+            <ProgressBarWrapper>
+              <ProgressBar></ProgressBar>
+            </ProgressBarWrapper>
+            <VideoDuration>
+              <CurrentDuration>2:20/</CurrentDuration>
+              <FullDuration>10:00</FullDuration>
+            </VideoDuration>
+            <img onClick={fullScreenRequest} src={fullScreenButton} />
+            <img src={screenMirroringButton} />
+            <img src={volumbeButton} />
+          </ControlBar>
+          <VideoElement
             onClick={handlePlayPauseClick}
-            src={!isPlaying ? playButton : pauseButton}
-          />
-          <ProgressBarWrapper>
-            <ProgressBar></ProgressBar>
-          </ProgressBarWrapper>
-          <Duration>
-            <CurrentDuration>2:20/</CurrentDuration>
-            <FullDuration>10:00</FullDuration>
-          </Duration>
-          <img onClick={fullScreenRequest} src={fullScreenButton} />
-          <img src={screenMirroringButton} />
-          <img src={volumbeButton} />
-        </ControlBar>
-        <VideoElement
-          onClick={handlePlayPauseClick}
-          ref={elementRef}
-          style={{ width: "100%" }}
-          src="https://download-video.akamaized.net/v2-1/playback/5f2ad7e4-0f3d-4cda-83fd-3aef3e8b23e0/62534a16?__token__=st=1690375053~exp=1690389453~acl=%2Fv2-1%2Fplayback%2F5f2ad7e4-0f3d-4cda-83fd-3aef3e8b23e0%2F62534a16%2A~hmac=920a4deb1c2131209722e35f33513345ac7aa0f82795c4d4d37de72de862e565&r=dXMtY2VudHJhbDE%3D"
-        ></VideoElement>
-      </VideoWrapper>
-    </div>
+            ref={elementRef}
+            style={{ width: "100%" }}
+            src="https://download-video.akamaized.net/v2-1/playback/5f2ad7e4-0f3d-4cda-83fd-3aef3e8b23e0/62534a16?__token__=st=1690375053~exp=1690389453~acl=%2Fv2-1%2Fplayback%2F5f2ad7e4-0f3d-4cda-83fd-3aef3e8b23e0%2F62534a16%2A~hmac=920a4deb1c2131209722e35f33513345ac7aa0f82795c4d4d37de72de862e565&r=dXMtY2VudHJhbDE%3D"
+          ></VideoElement>
+        </VideoWrapper>
+        <CourseInfo>
+          <Title>{targetCourse.title}</Title>
+          <MiniInfo>
+            <InstructorInfo>
+              <InstructorAvatar src={targetCourse.instructor_image} />
+              <InstructorName>{targetCourse.instructor_name}</InstructorName>
+              <InstructorJob>{targetCourse.instructor_job}</InstructorJob>
+              <FollowLink>follow mentor</FollowLink>
+            </InstructorInfo>
+            <CourseRating>{targetCourse.rating.toFixed(1)}</CourseRating>
+          </MiniInfo>
+        </CourseInfo>
+      </CourseWrapper>
+      <CourseCard>
+        <Title>{targetCourse.title}</Title>
+        <CardCourseInfo>
+          <InstructorAvatar src={targetCourse.instructor_image} />
+          <InstructorName>{targetCourse.instructor_name}</InstructorName>
+          <CourseCardRating image={star}>{targetCourse.rating.toFixed(1)}</CourseCardRating>
+        </CardCourseInfo>
+        <CourseDetails>
+          <Students image={user}>{targetCourse.students} Student</Students>
+          <Modules image={document}>{targetCourse.sections} Module</Modules>
+          <Duration image={clock}>{targetCourse.duration}</Duration>
+        </CourseDetails>
+      </CourseCard>
+    </FullCourseWrapper>
   );
 };
 
 export default Course;
 
+const FullCourseWrapper = styled.div`
+  display: grid;
+  column-gap: 30px;
+  grid-template-columns: 1fr ${350 / 16}rem;
+
+  & > * {
+    background-color: ${COLORS.neutral.white};
+  }
+`;
+
+const CourseWrapper = styled.div``;
 const VideoWrapper = styled.div`
   position: relative;
   border-radius: 1rem;
@@ -98,6 +140,8 @@ const VideoWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  max-width: ${744 / 16}rem;
+  max-height: ${500 / 16}rem;
 `;
 
 const VideoElement = styled.video`
@@ -149,9 +193,114 @@ const ProgressBar = styled.div`
   width: 40%;
   height: 100%;
 `;
-const Duration = styled.div`
+const VideoDuration = styled.div`
   font-family: ${FONT_FAMILY.asap};
   font-weight: 600;
 `;
 const CurrentDuration = styled.span``;
 const FullDuration = styled.span``;
+
+const CourseInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+const Title = styled.h2`
+  color: ${COLORS.neutral.black};
+  font-size: ${TYPOGRAPHY.xl2};
+`;
+const MiniInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const InstructorInfo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  font-size: ${TYPOGRAPHY.sm};
+  font-weight: normal;
+`;
+const InstructorAvatar = styled.img`
+  max-width: ${30 / 16}rem;
+  max-height: ${30 / 16}rem;
+  border-radius: 50%;
+`;
+const InstructorName = styled.h3`
+  font-weight: normal;
+  margin-right: auto;
+`;
+
+const InstructorJob = styled.h3`
+  color: ${COLORS.neutral.darkGrey};
+  font-weight: normal;
+`;
+
+const FollowLink = styled.button`
+  color: ${COLORS.seconday.blue};
+  text-transform: capitalize;
+  cursor: pointer;
+`;
+
+const CourseRating = styled.span`
+  color: ${COLORS.neutral.black};
+`;
+
+const CourseCard = styled.div`
+  --spacing: 30px;
+  display: flex;
+  flex-direction: column;
+  gap: var(--spacing);
+  padding: var(--spacing);
+
+  border-radius: 1rem;
+`;
+
+const CardCourseInfo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: ${10 / 16}rem;
+`;
+
+const CourseDetailsSingleItem = styled.span`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  color: ${COLORS.neutral.black};
+  font-size: ${TYPOGRAPHY.base};
+  font-weight: 500;
+
+  &::before {
+    content: "";
+    display: inline-block;
+    width: 1rem;
+    height: 1rem;
+  }
+`;
+
+const CourseCardRating = styled(CourseDetailsSingleItem)`
+  &::before {
+    background-image: url(${(props) => props.image && props.image});
+  }
+`;
+const CourseDetails = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Students = styled(CourseDetailsSingleItem)`
+  &::before {
+    background-image: url(${(props) => props.image && props.image});
+  }
+`;
+const Modules = styled(CourseDetailsSingleItem)`
+  &::before {
+    background-image: url(${(props) => props.image && props.image});
+  }
+`;
+const Duration = styled(CourseDetailsSingleItem)`
+  &::before {
+    background-image: url(${(props) => props.image && props.image});
+  }
+`;
